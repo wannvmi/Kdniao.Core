4bfc1ef52bb7ff0496bec4a91edec5c0b02446b3
pursue <996198546@qq.com>
2020-01-17 12:48:05 +0800

    create project

---
+++ src/Kdniao.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kdniao.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAlipay(
+            this IServiceCollection services)
+        {
+            //services.AddAlipay(null);
+        }
+    }
+}
